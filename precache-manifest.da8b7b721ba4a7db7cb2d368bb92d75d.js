ab2ce97a04d3d4eb6f2d0daa7eff942857acfcea
Bhavin2513 <bhavinpatel225.bp@gmail.com>
2020-05-07 20:58:54 +0530

    Updates

---
+++ precache-manifest.da8b7b721ba4a7db7cb2d368bb92d75d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630977efdb424e4e91e941cdd1d3d63e",
+    "url": "/react-todo1/index.html"
+  },
+  {
+    "revision": "0112088e109460dfe413",
+    "url": "/react-todo1/static/css/main.d540cd92.chunk.css"
+  },
+  {
+    "revision": "2987e9a823f3ba2edc53",
+    "url": "/react-todo1/static/js/2.a783e81c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo1/static/js/2.a783e81c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0112088e109460dfe413",
+    "url": "/react-todo1/static/js/main.19c4ef49.chunk.js"
+  },
+  {
+    "revision": "0b60f453aa162b2e5484",
+    "url": "/react-todo1/static/js/runtime-main.e84f661c.js"
+  }
+]);
